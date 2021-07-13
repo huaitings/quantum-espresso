4c7c5c3d1a73a123362ef87066bb51c0f2accc36
kevin5335711 <70958363+kevin5335711@users.noreply.github.com>
2021-07-13 12:22:05 +0800

    Update checkout.pl

--- checkout.pl
+++ checkout.pl
@@ -9,13 +9,13 @@ my $myelement = join ('',@myelement);
 my $out_file = `find $currentPath/$myelement/Opt -name "*.sout"`;
 my @out_file = split("\n", $out_file);
 @out_file = sort @out_file; 
-my @out_filename = map (($_ =~ m/(\w+-\w+-\w+).sout$/gm),@out_file);
+my @out_filename = map (($_ =~ m/.*\/(.*).sout$/gm),@out_file);
 
 
 my $folder = `find $currentPath/$myelement/Opt -type d -name "*-*-*"`;
 my @folder = split("\n", $folder);
 @folder = sort @folder;
-my @foldername = map (($_ =~ m/(\w*\.?\w*[+-]*\w+-\w+-\w+)$/gm),@folder);
+my @foldername = map (($_ =~ m/.*\/(.*)$/gm),@folder);
 
 
 my $running = `squeue -o \%j | awk 'NR!=1'`;
@@ -24,6 +24,7 @@ my %running;
 for(@running){
     $running{$_} = 1;
 }
+
 
 
 print "DONE : \n";
@@ -37,26 +38,24 @@ for my $id1 (0..$#out_file){
 }
 
 print "QE calculating : \n";
-for my $id2 (0..$#out_file){
-    if( exists $running{$out_filename[$id2]}){
-      print "\t$out_filename[$id2]\n";
+for my $id2 (0..$#foldername){
+    if( exists $running{$foldername[$id2]}){
+      print "\t$foldername[$id2]\n";
     }
 }
 
 print "Not summited : \n";
 for my $id3(0..$#folder){
-    if (-e "$folder[$id3]/$foldername[$id3].sout" ){
+  if( exists $running{$foldername[$id3]}){
+    next;
+  }
+  if (-e "$folder[$id3]/$foldername[$id3].sout" ){
         my $done = `grep -o -a 'DONE' $folder[$id3]/$foldername[$id3].sout`; 
         chomp $done;
         if( $done eq "DONE" ){
             next;
-        }else{
-          print "\t$foldername[$id3]\n";
-          next;
         }
-    }
-    if( exists $running{$foldername[$id3]}){
-      next;
-    }
+  }
+
     print "\t$foldername[$id3]\n";
 } 
